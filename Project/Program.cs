cdd995becbbc0160629b21d91e57bd336173be5e
IBXCODECAT <nathan.schmitt7@gmail.com>
2022-05-17 21:06:27 -0500

    Englishify Error Messages

--- Project/Program.cs
+++ Project/Program.cs
@@ -17,16 +17,19 @@ namespace Decrapify_Windows11
         /// <summary>
         /// Starts the process at the path provided
         /// </summary>
-        /// <param name="path">The full path to the process to invoke</param>
+        /// <param name="processName">The name of the process to invoke</param>
         /// <returns>The exit code provided by the executable if availible</returns>
-        private static int StartProcess(string path)
+        private static int StartProcess(string processName)
         {
-            if(File.Exists(path))
+            string searchPath = AppDomain.CurrentDomain.BaseDirectory;
+            string fullPath = searchPath + processName;
+
+            if (File.Exists(fullPath))
             {
                 try
                 {
                     Process process = new Process();
-                    process.StartInfo.FileName = path;
+                    process.StartInfo.FileName = fullPath;
                     process.EnableRaisingEvents = true;
 
                     process.Start(); //Invoke..
@@ -37,14 +40,15 @@ namespace Decrapify_Windows11
                 }
                 catch
                 {
-                    Popup.CreatePopup("Failed to Invoke Process", "The application failed to invoke the process `" + path + "` due to an exception.", Popup.Options.ok, Popup.Icons.error);
+                    Popup.CreatePopup("Failed to Invoke Process", "The application failed to invoke the process `" + processName + "` due to an exception.", Popup.Options.ok, Popup.Icons.error);
                 }
             }
             else
             {
-                Popup.CreatePopup("Failed to Invoke Process", "The application failed to invoke the process `" + path + "` because it does not exist.", Popup.Options.ok, Popup.Icons.error);
+                Popup.CreatePopup("Failed to Invoke Process", "The application failed to invoke the process `" + processName + "` because it does not exist.", Popup.Options.ok, Popup.Icons.error);
             }
 
+            Console.WriteLine("Failed to Invoke Process '" + processName + "' at '" + fullPath + "'. Verify that the required files are in the same directory as the executable.");
             return -1;
         }
     }
