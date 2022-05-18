86efad660964435a69289c69f7bc5ab3bed80081
IBXCODECAT <nathan.schmitt7@gmail.com>
2022-05-17 20:14:18 -0500

    Create MessageBox & Skeleton

---
+++ DLL/WindowsUtilities/Popup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace IBX_Plugins.WindowsUtilities
+{
+    /// <summary>
+    /// A class for creating a Windows Popup Dialog |
+    /// For more info see: <see>https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-messagebox</see>
+    /// </summary>
+    public static class Popup
+    {
+        public struct interupt
+        {
+            public static long app = 0x00000000L;
+            public static long sys = 0x00001000L;
+            public static long task = 0x00002000L;
+            public static long help = 0x00004000L;
+        }
+
+        public struct Icons
+        {
+            public static long error = 0x00000010L;
+            public static long querry = 0x00000020L;
+            public static long warn = 0x00000030L;
+            public static long info = 0x00000040L;
+        }
+
+        public struct Options
+        {
+            public static long ok = 0x00000000L;
+            public static long okCancel = 0x00000001L;
+            public static long abortRetryIgnore = 0x00000002L;
+            public static long yesNoCancel = 0x00000003L;
+            public static long yesNo = 0x00000004L;
+            public static long retryCancel = 0x00000005L;
+            public static long cancelRetryContinue = 0x00000006L;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetActiveWindow();
+
+        /// <summary>
+        /// Get the memory pointer of the active window handle
+        /// </summary>
+        /// <returns>A 32-bit memory pointer to the active window handle</returns>
+        public static IntPtr GetWindowHandle() { return GetActiveWindow(); }
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int MessageBox(IntPtr hwnd, String lpText, String lpCaption, uint uType);
+
+        /// <summary>
+        /// Create a Windows Dialog Box popup window. WARNING: This code is blocking and may temperarily suspend execution
+        /// </summary>
+        /// <param name="title">The text that is displayed in the title bar of the popup window</param>
+        /// <param name="text">The text that is displayed within the popup window</param>
+        /// <param name="options">The options that are availible in the options bar (long code, see docs)</param>
+        /// <param name="icon">The icon shown to the left of the text in the popup window (long code, see docs)</param>
+        /// <returns>An integer representing the option the user has chosen</returns>
+        public static int CreatePopup(string title, string text, long options, long icon)
+        {
+            try
+            {
+                return MessageBox(GetWindowHandle(), text, title, (uint)(options | icon));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return -1;
+            }
+        }
+    }
+}
---
+++ Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Decrapify-Windows11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Decrapify-Windows11")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a84c60d6-f7d7-4497-b76f-eb8cac412bbc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
